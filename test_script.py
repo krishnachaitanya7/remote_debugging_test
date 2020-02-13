6e1dbdbc29b9b5b54d68be6099c15a2b46e5f509
krishnachaitanya9 <chaitanyaradon89@gmail.com>
2020-02-12 18:01:25 -0700

    Added pydevd

---
+++ test_script.py
@@ -0,0 +1,13 @@
+"""
+Remote debugging setup
+Reference: https://stackoverflow.com/questions/6989965/how-do-i-start-up-remote-debugging-with-pycharm
+"""
+import math
+import pydevd_pycharm
+pydevd_pycharm.settrace('172.21.39.44', port=4444, stdoutToServer=True, stderrToServer=True)
+
+if __name__ == "__main__":
+    x = 0
+    print("Wantedly testing remote exception to see if it works or not")
+    y = 2/x
+    print("Yayy!!!")
